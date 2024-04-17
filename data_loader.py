1128e7fb1686a2b1788397865e6cc8ee84c85948
Amit Aaranya <92247041+AmitAaranya@users.noreply.github.com>
2024-04-17 06:03:40 +0000

    Made the Image size Variable

--- data_loader.py
+++ data_loader.py
@@ -4,14 +4,14 @@ from PIL import Image
 import torchvision.transforms as transforms
 
 
-def load_datasets(folder = "datasets",train_or_test_folder = "Train_Alphabet"):
+def load_datasets(folder = "datasets",train_or_test_folder = "Train_Alphabet",size = 50):
     x, labels = [],[]
     train_dataset_folder = os.path.join(folder,train_or_test_folder)
     for each_label in sorted(os.listdir(train_dataset_folder)):
         lable_folder = os.path.join(train_dataset_folder,each_label)
         for file_name in os.listdir(lable_folder):
             file_path = os.path.join(lable_folder,file_name)
-            image_tensor = image_to_tensor(load_image(filename=file_path,size = 50)) 
+            image_tensor = image_to_tensor(load_image(filename=file_path,size = size)) 
             x.append(image_tensor)
             labels.append(each_label)
     return np.array(x),np.array(labels)
