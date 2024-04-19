f31227f64d7ed4b7e565dda49bcecadea458e3c3
Amit Aaranya <92247041+AmitAaranya@users.noreply.github.com>
2024-04-19 12:03:30 +0000

    Added the print to see the logs of data load process

--- data_loader.py
+++ data_loader.py
@@ -7,6 +7,7 @@ import torchvision.transforms as transforms
 def load_datasets(folder = "datasets",train_or_test_folder = "Train_Alphabet",size = 50):
     x, labels = [],[]
     train_dataset_folder = os.path.join(folder,train_or_test_folder)
+    print("Loading datasets: ",end = " ")
     for each_label in sorted(os.listdir(train_dataset_folder)):
         lable_folder = os.path.join(train_dataset_folder,each_label)
         for file_name in os.listdir(lable_folder):
@@ -14,6 +15,8 @@ def load_datasets(folder = "datasets",train_or_test_folder = "Train_Alphabet",si
             image_tensor = image_to_tensor(load_image(filename=file_path,size = size)) 
             x.append(image_tensor)
             labels.append(each_label)
+        print(each_label,end = " ")
+    print("\n")
     return np.array(x),np.array(labels)
 
 def load_image(filename,size):
